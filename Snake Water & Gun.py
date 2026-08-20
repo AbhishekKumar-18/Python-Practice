1b10c9eeb7b22b8b19d53f249ea810cadf70b16d
Abhishek Kumar <abhishekshandilya2006@gmail.com>
2026-08-20 15:47:55 +0530

    Added Snake Water Gun game using functions

---
+++ Snake Water & Gun.py
@@ -0,0 +1,28 @@
+import random
+
+def check(computer,user):
+    if computer==user:
+        return 0
+
+    if(computer == 0 and user == 1):
+        return -1
+
+    if(computer == 1 and user == 2):
+        return -1
+
+    if(computer == 2 and user == 0):
+        return -1
+
+    return 1
+
+
+computer = random.randint(0,2)
+user=int(input("0 For Snake, 1 For Water and 2 For Gun\n"))
+
+score = check(computer,user)
+if score == 0:
+    print("Its a Draw")
+elif score == -1:
+    print("You Lose")
+else:
+    print("You Won")
